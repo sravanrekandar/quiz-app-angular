2bab3610301c4248f2c8fe8df9102b1e5a8a2b63
sravanrekandar <sravanrekandar@gmail.com>
2014-08-31 16:16:47 +0530

    Enhancements

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 1405);
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.bodyParser());
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+    process.stdout.write('\x07');
+});
